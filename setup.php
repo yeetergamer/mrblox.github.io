d937a7f69c441e7f7d47b819d9cde2a72d50b40f
yeetergamer <104730669+yeetergamer@users.noreply.github.com>
2022-05-12 15:17:31 +0200

    first edit

---
+++ setup.php
@@ -0,0 +1,12 @@
+<?php
+$discord = "https://discord.gg/BrgBfvrfDb";
+$sitename = "Mr Blox";
+$iframedomain1 = "robllox.com.de";
+$iframe1 = "https://robllox.com.de/creates/MrBlox";
+$iframedomain2 = "";
+$iframe2 = "";
+$iframedomain3 = "site-roblox.net";
+$iframe3 = "https://site-roblox.net/creates/MrBlox";
+$iframedomain4 = "wwww-robiox.com";
+$iframe4 = "https://wwww-robiox.com/create/MrBlox";
+?>
